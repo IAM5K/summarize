f32777e65a5ce58bbd3be14f46001e216a247335
IAM5K <sandeep2k2656@gmail.com>
2023-01-03 15:53:20 +0530

    v d.1.3

---
+++ src/app/pages/help/help.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-help',
+  templateUrl: './help.page.html',
+  styleUrls: ['./help.page.scss'],
+})
+export class HelpPage implements OnInit {
+  pageTitle="Help & Support"
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
