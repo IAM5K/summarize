4b3ff35ec5e1a3421d3fa53c299141e2cae6352e
IAM5K <sandeep2k2656@gmail.com>
2023-01-03 15:52:44 +0530

    v d.1.2

--- src/app/pages/office/office.page.ts
+++ src/app/pages/office/office.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { serverTimestamp } from '@angular/fire/firestore';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { OfficeService } from 'src/app/services/office/office.service';
 @Component({
   selector: 'app-office',
   templateUrl: './office.page.html',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OfficePage implements OnInit {
   pageTitle = "Office"
-  constructor() { }
-
+  Works:any=[];
+  worksCount:number=0
+  constructor(
+    private fb: FormBuilder,
+    private officeService: OfficeService
+  ) { }
+  workForm: FormGroup = this.fb.group({
+    createdAt: [serverTimestamp()],
+    date: [new Date(), [Validators.required, Validators.pattern('^[a-zA-Z 0-9 .,-]*$')]],
+    startTime: ['', [Validators.required, Validators.pattern('^[a-zA-Z 0-9 .,-]*$')]],
+    endTime: ['', [Validators.required, Validators.pattern('^[a-zA-Z 0-9 .,-]*$')]],
+    type: ['coding', [Validators.required, Validators.pattern('^[a-zA-Z 0-9 :/.,-]*$')]],
+    description: ['', [Validators.required, Validators.pattern('^[a-zA-Z 0-9 .,-]*$')]],
+    // spendedOn: ['self', [Validators.required, Validators.pattern('^[a-zA-Z 0-9 .,-]*$')]],
+    updatedAt: [serverTimestamp()]
+  })
   ngOnInit() {
+    this.getWork()
+  }
+
+  async getWork(){
+    await this.officeService.getWork().subscribe(res=>{
+      this.Works = res
+      this.worksCount = this.Works.length
+    })
+
+  }
+  addWork() {
+    // console.log(this.workForm.value);
+    this.officeService.addWork(this.workForm.value)
   }
 
 }
