599ad4836896ecb49fc8997b10049856bf6da33f
IAM5K <sandeep2k2656@gmail.com>
2023-01-05 22:30:10 +0530

    d.1.4

--- src/app/services/studies/studies.service.ts
+++ src/app/services/studies/studies.service.ts
@@ -1,9 +1,48 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AlertController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StudiesService {
 
-  constructor() { }
+  constructor(
+  private afs: AngularFirestore,
+  private alertCtrl: AlertController)
+  { }
+  studiesCollection = this.afs.collection('userData')
+  addStudies(data: any) {
+    let userId = ""
+    let userData = localStorage.getItem('UserData')
+    if (userData) {
+      userId = JSON.parse(userData).uid
+    }
+    this.studiesCollection.doc(userId).collection('myStudies').add(data).then(res => {
+      this.successAlert();
+    }).catch(err => {
+      alert("There was an error in posting. \n Please try again later. Check console for detail.");
+      console.warn(err);
+    })
+  }
+  getStudies() {
+    let userId = ""
+    let userData = localStorage.getItem('UserData')
+    if (userData) {
+      userId = JSON.parse(userData).uid
+    }
+    return this.studiesCollection.doc(userId).collection('myStudies', ref => ref.orderBy('date', 'desc')).valueChanges({ idField: 'idField' })
+  }
+
+  async successAlert() {
+    const alert = await this.alertCtrl.create({
+      header: 'Success',
+      subHeader: 'Studies added Successfully!',
+      cssClass: 'success-alert',
+      // message: 'This is an alert!',
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
 }
