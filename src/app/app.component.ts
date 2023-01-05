13a7c3b41c75257be157ad30792c755d87c4f76a
IAM5K <sandeep2k2656@gmail.com>
2023-01-05 22:31:28 +0530

    d.1.5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
 import { AuthService } from './services/auth/auth.service';
+import { StatusBar, Style } from '@capacitor/status-bar';
+import { isPlatform } from '@ionic/angular';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -9,18 +11,21 @@ import { AuthService } from './services/auth/auth.service';
 })
 export class AppComponent {
   isLoggedIn: any = false;
-  public appPages:any = [];
-  public labels:any = [];
+  public appPages: any = [];
+  public labels: any = [];
   constructor(
     private authService: AuthService,
     private firebaseAuth: AngularFireAuth,
-		private router: Router) {
+    private router: Router) {
   }
 
   ngOnInit() {
+    if (isPlatform('mobile')) {
+      StatusBar.setBackgroundColor({color:"#8020A0"}).catch(()=>{});
+    }
     this.getUser()
   }
-  afterViewinit(): void{
+  afterViewinit(): void {
     if (this.isLoggedIn) {
       this.getSidebar()
     }
@@ -38,22 +43,23 @@ export class AppComponent {
     });
   }
 
-  getSidebar(){
+  getSidebar() {
     this.appPages = [
       { title: 'Dashboard', color: 'primary', url: 'home', icon: 'grid' },
       { title: 'About', color: 'warning', url: 'about', icon: 'reader' },
       { title: 'Expenses', color: 'success', url: 'expenses', icon: 'cash' },
       { title: 'Time', color: 'danger', url: 'work', icon: 'hourglass' },
+      { title: 'Studies', color: 'primary', url: 'studies', icon: 'book' },
       // { title: 'Analytics', color: 'success', url: 'analytics', icon: 'analytics' },
       // { title: 'Setup', color: 'warning', url: 'setup', icon: 'settings' },
       { title: 'Profile', color: 'secondary', url: 'profile', icon: 'person' },
-      { title: 'Need help', color: 'tertiary', url: 'help', icon: 'help-circle'}
+      { title: 'Need help', color: 'tertiary', url: 'help', icon: 'help-circle' }
     ];
   }
 
-  async logout(){
+  async logout() {
     await this.authService.logout();
-    this.router.navigateByUrl('login', { replaceUrl:true });
+    this.router.navigateByUrl('login', { replaceUrl: true });
   }
 
 }
