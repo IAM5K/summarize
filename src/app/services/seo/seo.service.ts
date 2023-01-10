e5d17244622c8ff8d498e2d6ad53a61b0ff34bc0
IAM5K <sandeep2k2656@gmail.com>
2023-01-10 12:28:20 +0530

    d.1.7-4

--- src/app/services/seo/seo.service.ts
+++ src/app/services/seo/seo.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
+import { GoogleTagManagerService } from 'angular-google-tag-manager';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SeoService {
 
-  constructor() { }
+  constructor(
+    private titleService: Title,
+    private metaTags: Meta,
+    private gtmService: GoogleTagManagerService
+  ) { }
+
+  seo(pageTitle: any, metaTag: any) {
+    this.titleService.setTitle(pageTitle)
+    this.metaTags.addTags(metaTag)
+  }
+
+  eventTrigger(eventName: string, pageName: string) {
+    const gtmTag = {
+      event: eventName,
+      pageName: pageName
+    };
+    this.gtmService.pushTag(gtmTag);
+  }
 }
