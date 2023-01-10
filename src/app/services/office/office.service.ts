e4e7676ccf21c5569885ab6aa95fc0dbf7ce4519
IAM5K <sandeep2k2656@gmail.com>
2023-01-10 12:56:48 +0530

    d.1.7-8

--- src/app/services/office/office.service.ts
+++ src/app/services/office/office.service.ts
@@ -9,40 +9,59 @@ export class OfficeService {
 
   userData = localStorage.getItem('UserData')
   constructor(
-    private afs:AngularFirestore,
-    private alertCtrl:AlertController) { }
+    private afs: AngularFirestore,
+    private alertCtrl: AlertController) { }
 
-    workCollection = this.afs.collection('userData')
-  getWork(){
-    let userId=""
-    if(this.userData){
-      userId = JSON.parse(this.userData).uid
+  successMessage = "Time Log Added Successfully!"
+  deletedMessage = "Time Log Deleted Successfully!"
+  workCollection = this.afs.collection('userData')
+  getUserId() {
+    let userId = ""
+    let userData = localStorage.getItem('UserData')
+    if (userData) {
+      userId = JSON.parse(userData).uid
     }
-    return this.workCollection.doc(userId).collection('myWork',ref => ref.orderBy('date', 'desc')).valueChanges({idField:'idField'})
+    return userId;
   }
-  addWork(data:any){
-    let userId=""
-    if(this.userData){
+  getWork(count: number) {
+    if (count > 4) {
+      return this.workCollection.doc(this.getUserId()).collection('myWork', ref => ref.orderBy('date', 'desc').limit(count)).valueChanges({ idField: 'idField' })
+    }
+    else {
+      return this.workCollection.doc(this.getUserId()).collection('myWork', ref => ref.orderBy('date', 'desc')).valueChanges({ idField: 'idField' })
+    }
+  }
+  addWork(data: any) {
+    let userId = ""
+    if (this.userData) {
       userId = JSON.parse(this.userData).uid
     }
-    this.workCollection.doc(userId).collection('myWork').add(data).then(res=>{
-      this.successAlert();
-    }).catch(err=>{
+    this.workCollection.doc(userId).collection('myWork').add(data).then(res => {
+      this.successAlert(this.successMessage);
+    }).catch(err => {
       alert("There was an error in posting. \n Please try again later. Check console for detail.");
       console.warn(err);
     })
   }
-  updateWork(data:any){
+  updateWork(data: any) {
 
   }
 
-  getWorkById(){}
-  deleteWork(){}
+  getWorkById() { }
+  deleteWork(idField: string) {
+    this.workCollection.doc(this.getUserId()).collection('myWork').doc(idField).delete().then(
+      () => {
+        this.successAlert(this.deletedMessage)
+      }
+    ).catch(err => {
+      alert(err)
+    })
+  }
 
-  async successAlert() {
+  async successAlert(message: string) {
     const alert = await this.alertCtrl.create({
       header: 'Success',
-      subHeader: 'Work added Successfully!',
+      subHeader: message,
       cssClass: 'success-alert',
       // message: 'This is an alert!',
       buttons: ['OK'],
