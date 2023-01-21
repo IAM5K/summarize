d40157d609cb37edfb20076cee81b4a4cdd74845
IAM5K <sandeep2k2656@gmail.com>
2023-01-22 01:32:40 +0530

    d.1.9-2

---
+++ src/app/services/support/support.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument} from '@angular/fire/compat/firestore';
+import { AlertController } from '@ionic/angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SupportService {
+
+  supportCollection: AngularFirestoreCollection;
+  supportDoc!: AngularFirestoreDocument;
+
+
+  constructor(
+    private afs:AngularFirestore
+  ) {
+    this.supportCollection=this.afs.collection('support',ref => ref.orderBy('createdAt', 'desc'))
+  }
+
+  postSupport(value:any){
+    return this.supportCollection.add(value)
+  }
+  getSupport(){
+    return this.supportCollection.valueChanges({idField:'idField'});
+  }
+  updateData(idField:string,data:any){
+    this.afs.doc(`support/${idField}`).update(data)
+    .then(() => console.log('success') )
+    .catch(err => console.log(err) )
+  }
+  deleteSupport(itemId:string,){
+    this.supportDoc= this.afs.doc(`support/${itemId}`)
+    return this.supportDoc.delete()
+  }
+
+
+  formatDate(str:any){
+    if(str == (null|| undefined)){
+      return false
+    }
+    else{
+      return str.toDate().toString().slice(4, 24)
+    }
+  }
+}
