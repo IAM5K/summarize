4042fe397d949ad7b4ab3c9847961d52b52223bd
IAM5K <sandeep2k2656@gmail.com>
2023-01-23 21:24:13 +0530

    p.2.0-2

---
+++ src/app/models/class/date/custom-date.ts
@@ -0,0 +1,49 @@
+export class CustomDate {
+  constructor() { }
+  public dateToday: any = this.getDateToday()
+
+  getDateToday() {
+    const year = new Date().getFullYear()
+    const date = new Date().getDate()
+    let month = new Date().getMonth() + 1;
+    let value = ""
+    if (month < 10) {
+      value = year + "-0" + month + "-" + date
+    }
+    else {
+      value = year + "-" + month + "-" + date
+    }
+    return value
+  }
+  getWeekBackDate() {
+    var date = new Date();
+    var last = new Date(date.getTime() - (7 * 24 * 60 * 60 * 1000));
+    var day = last.getDate();
+    var month = last.getMonth() + 1;
+    var year = last.getFullYear();
+    var value = ""
+    if (month < 10) {
+      value = year + "-0" + month + "-" + day
+    }
+    else {
+      value = year + "-" + month + "-" + day
+    }
+    return value
+  }
+  getLastMonthDate() {
+    var date = new Date();
+    var last = new Date(date.getTime() - (30 * 24 * 60 * 60 * 1000));
+    var day = last.getDate();
+    var month = last.getMonth() + 1;
+    var year = last.getFullYear();
+    var value = ""
+    if (month < 10) {
+      value = year + "-0" + month + "-" + day
+    }
+    else {
+      value = year + "-" + month + "-" + day
+    }
+    return value
+  }
+
+}
