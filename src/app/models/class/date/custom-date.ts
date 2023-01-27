722c5347869fcb975d2a5bd6d42d5779a57c174e
IAM5K <sandeep2k2656@gmail.com>
2023-01-27 10:35:58 +0530

    d.2.0-4

--- src/app/models/class/date/custom-date.ts
+++ src/app/models/class/date/custom-date.ts
@@ -45,5 +45,35 @@ export class CustomDate {
     }
     return value
   }
+  getLastYearDate() {
+    var date = new Date();
+    var last = new Date(date.getTime() - (365 * 24 * 60 * 60 * 1000));
+    var day = last.getDate();
+    var month = last.getMonth() + 1;
+    var year = last.getFullYear();
+    var value = ""
+    if (month < 10) {
+      value = year + "-0" + month + "-" + day
+    }
+    else {
+      value = year + "-" + month + "-" + day
+    }
+    return value
+  }
+  getThisYear(){
+    return new Date().getFullYear() + "-01" + "-01"
+  }
+  getThisMonth(){
+    const year = new Date().getFullYear()
+    let month = new Date().getMonth() + 1;
+    let value = ""
+    if (month < 10) {
+      value = year + "-0" + month + "-01"
+    }
+    else {
+      value = year + "-" + month + "-01"
+    }
+    return value
+  }
 
 }
