190251f66e46f9012ae1f84c03d34d68c4664a05
IAM5K <sandeep2k2656@gmail.com>
2023-02-11 22:53:54 +0530

    d.2.3-12

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,84 +15,71 @@ const routes: Routes = [
   },
   {
     path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule),
+    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule),
     ...canActivate(redirectLoggedInToHome)
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule),
-    ...canActivate(redirectUnauthorizedToLogin)
-
-  },
-  {
-    path: 'analytics',
-    loadChildren: () => import('./pages/analytics/analytics.module').then( m => m.AnalyticsPageModule),
-    ...canActivate(redirectUnauthorizedToLogin)
-
-  },
-  {
-    path: 'expenses',
-    loadChildren: () => import('./pages/expense/expenses.module').then( m => m.ExpensesPageModule),
-    ...canActivate(redirectUnauthorizedToLogin)
-
-  },
-  {
     path: 'home',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
-
-  },
-  {
-    path: 'work',
-    loadChildren: () => import('./pages/office/office.module').then( m => m.OfficePageModule),
-    ...canActivate(redirectUnauthorizedToLogin)
-
-  },
-  {
-    path: 'setup',
-    loadChildren: () => import('./pages/setup/setup.module').then( m => m.SetupPageModule),
-    ...canActivate(redirectUnauthorizedToLogin)
-
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule)
   },
   {
     path: 'help',
-    loadChildren: () => import('./pages/help/help.module').then( m => m.HelpPageModule)
-  },
-  {
-    path: 'support',
-    loadChildren: () => import('./pages/support/support.module').then( m => m.SupportPageModule),
-    ...canActivate(redirectUnauthorizedToLogin)
-
-  },
-  {
-    path: 'profile',
-    loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule),
-    ...canActivate(redirectUnauthorizedToLogin)
-
+    loadChildren: () => import('./pages/help/help.module').then(m => m.HelpPageModule)
   },
   {
     path: 'about',
-    loadChildren: () => import('./pages/about/about.module').then( m => m.AboutPageModule)
+    loadChildren: () => import('./pages/about/about.module').then(m => m.AboutPageModule)
+  },
+  {
+    path: 'expenses',
+    loadChildren: () => import('./pages/expense/expenses.module').then(m => m.ExpensesPageModule),
+    ...canActivate(redirectUnauthorizedToLogin)
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./pages/profile/profile.module').then(m => m.ProfilePageModule),
+    ...canActivate(redirectUnauthorizedToLogin)
   },
   {
     path: 'studies',
-    loadChildren: () => import('./pages/studies/studies.module').then( m => m.StudiesPageModule),
+    loadChildren: () => import('./pages/studies/studies.module').then(m => m.StudiesPageModule),
     ...canActivate(redirectUnauthorizedToLogin)
   },
   {
     path: 'achievement',
-    loadChildren: () => import('./pages/achievement/achievement.module').then( m => m.AchievementPageModule),
+    loadChildren: () => import('./pages/achievement/achievement.module').then(m => m.AchievementPageModule),
     ...canActivate(redirectUnauthorizedToLogin)
   },
   {
     path: 'time',
-    loadChildren: () => import('./pages/time/time.module').then( m => m.TimePageModule),
+    loadChildren: () => import('./pages/time/time.module').then(m => m.TimePageModule),
     ...canActivate(redirectUnauthorizedToLogin)
   },
-	{
-		path: '**',
-		redirectTo: 'home',
-		pathMatch: 'full'
-	}
+  // {
+  //   path: 'folder/:id',
+  //   loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule),
+  //   ...canActivate(redirectUnauthorizedToLogin)
+  // },
+  // {
+  //   path: 'analytics',
+  //   loadChildren: () => import('./pages/analytics/analytics.module').then( m => m.AnalyticsPageModule),
+  //   ...canActivate(redirectUnauthorizedToLogin)
+  // },
+  // {
+  //   path: 'work',
+  //   loadChildren: () => import('./pages/office/office.module').then( m => m.OfficePageModule),
+  //   ...canActivate(redirectUnauthorizedToLogin)
+  // },
+  // {
+  //   path: 'setup',
+  //   loadChildren: () => import('./pages/setup/setup.module').then( m => m.SetupPageModule),
+  //   ...canActivate(redirectUnauthorizedToLogin)
+  // },
+  {
+    path: '**',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
 ];
 
 @NgModule({
@@ -101,4 +88,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
