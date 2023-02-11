7f3904176a9a79d5dff640111aa0d68d5e710c0a
IAM5K <sandeep2k2656@gmail.com>
2023-02-11 20:28:17 +0530

    d.2.3-9

---
+++ src/app/pages/studies/components/edit-studies/edit-studies.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { EditStudiesComponent } from './edit-studies.component';
+
+describe('EditStudiesComponent', () => {
+  let component: EditStudiesComponent;
+  let fixture: ComponentFixture<EditStudiesComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ EditStudiesComponent ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(EditStudiesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
