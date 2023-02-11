0ae67694d5d29fea5a81995a3bd1a451fb196533
IAM5K <sandeep2k2656@gmail.com>
2023-02-11 20:25:21 +0530

    d.2.3-8

--- src/app/services/studies/studies.service.ts
+++ src/app/services/studies/studies.service.ts
@@ -14,11 +14,13 @@ export class StudiesService {
     private profileService: ProfileService
   ) { }
 
+  addMessage = "Studies added successfully."
+  deletedMessage = "Studies has been successfully deleted."
   userId = this.profileService.getUserProfile()?.uid
   studiesCollection = this.afs.collection('userData')
   addStudies(data: any) {
     this.studiesCollection.doc(this.userId).collection('myStudies').add(data).then(res => {
-      this.successAlert();
+      this.successAlert( this.addMessage );
     }).catch(err => {
       alert("There was an error in posting. \n Please try again later. Check console for detail.");
       console.warn(err);
@@ -28,10 +30,20 @@ export class StudiesService {
     return this.studiesCollection.doc(this.userId).collection('myStudies', ref => ref.orderBy('date', 'desc')).valueChanges({ idField: 'idField' })
   }
 
-  async successAlert() {
+  deleteStudies(idField: string) {
+    this.studiesCollection.doc(this.userId).collection('myStudies').doc(idField).delete().then(
+      () => {
+        this.successAlert(this.deletedMessage)
+      }
+    ).catch(err => {
+      alert(err)
+    })
+  }
+
+  async successAlert(message:string) {
     const alert = await this.alertCtrl.create({
       header: 'Success',
-      subHeader: 'Studies added Successfully!',
+      subHeader: message,
       cssClass: 'success-alert',
       // message: 'This is an alert!',
       buttons: ['OK'],
