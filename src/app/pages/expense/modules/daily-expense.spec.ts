3d964b12bc4542f1f6dd1dbc074544835e1a3511
IAM5K <sandeep2k2656@gmail.com>
2023-05-17 15:59:40 +0530

    d.2.7-4

---
+++ src/app/pages/expense/modules/daily-expense.spec.ts
@@ -0,0 +1,7 @@
+import { DailyExpense } from './daily-expense';
+
+describe('DailyExpense', () => {
+  it('should create an instance', () => {
+    expect(new DailyExpense()).toBeTruthy();
+  });
+});
