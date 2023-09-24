3eb56a2372b9ca6e5a0a416be36b521fcf98f42e
IAM5K <sandeep2k2656@gmail.com>
2023-09-24 10:06:05 +0530

    d.4.0-3

--- src/app/pages/studies/studies.page.ts
+++ src/app/pages/studies/studies.page.ts
@@ -13,24 +13,6 @@ import { StudiesService } from 'src/app/services/studies/studies.service';
   styleUrls: ['./studies.page.scss']
 })
 export class StudiesPage implements OnInit {
-  pageTitle = "Studies";
-  pageMetaTags = [
-    {
-      name: 'description',
-      content: "Summarize all your expenses here. Summarize will help you to check them down in the list immediately and later Analyze them to have an understanding about where you can spend wisely and how to manage your expenses in better way. Soon we will also give finance tips that will help you better."
-    },
-    {
-      name: 'keyword',
-      content: 'Summarize, Summarize, arise, arize, money managemnet, expense management, cost analysis,summarize-ng, summarize-ng, digital dairy, expense analysis'
-    },
-    {
-      name: 'author',
-      content: 'Sandeep Kumar'
-    }
-  ];
-  Studies: any = [];
-  studiesCount: number = 0;
-  currentTime = this.datePipe.transform(new Date(), 'hh:mm');
   constructor(
     private fb: FormBuilder,
     private studiesService: StudiesService,
@@ -40,6 +22,27 @@ export class StudiesPage implements OnInit {
     private viewportScroller: ViewportScroller,
     private profileService: ProfileService
   ) { }
+
+  pageTitle = "Studies";
+  pageMetaTags = [
+    {
+      name: 'description',
+      content: "Summarize all your expenses here. Summarize will help you to check them down in the list immediately and later Analyze them to have an understanding about where you can spend wisely and how to manage your expenses in better way. Soon we will also give finance tips that will help you better."
+    },
+    {
+      name: 'keyword',
+      content: 'Summarize, Summarize, arise, arize, money management, expense management, cost analysis,summarize-ng, summarize-ng, digital dairy, expense analysis'
+    },
+    {
+      name: 'author',
+      content: 'Sandeep Kumar'
+    }
+  ];
+  Studies: any = [];
+  studiesCount: number = 0;
+  currentTime = this.datePipe.transform(new Date(), 'hh:mm');
+  advancedMode:boolean = true;
+  advancedModeAvailable:boolean = false;
   editMode:boolean = false;
   dateToday: string | null = this.datePipe.transform(new Date(), 'yyyy-MM-dd');
   studiesForm: FormGroup = this.fb.group({
@@ -64,10 +67,8 @@ export class StudiesPage implements OnInit {
   updateDataId:string ="";
   ngOnInit() {
     this.getStudies()
-    this.seoService.seo(this.pageTitle, this.pageMetaTags)
-    const educationDetails = this.profileService.getEducationalDetail();
-    console.log(educationDetails);
-    
+    this.seoService.seo(this.pageTitle, this.pageMetaTags);
+    this.activateAdvancedMode();
   }
 
   async getStudies() {
@@ -131,4 +132,12 @@ export class StudiesPage implements OnInit {
     this.editMode = true;
   }
 
+  async activateAdvancedMode(){
+    const profileData = await this.profileService.getProfileData();
+    if(profileData.educationDetails){
+      this.advancedModeAvailable = true
+      this.advancedMode = true;
+      console.log(profileData);      
+    } 
+  }
 }
