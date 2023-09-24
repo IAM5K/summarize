b37b21575307ad3a623d9f7b9791cff8f6166ff3
IAM5K <sandeep2k2656@gmail.com>
2023-09-24 10:04:28 +0530

    d.4.0-2

---
+++ src/app/pages/studies/components/advanced-studies/advanced-studies.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-advanced-studies',
+  templateUrl: './advanced-studies.component.html',
+  styleUrls: ['./advanced-studies.component.scss'],
+})
+export class AdvancedStudiesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
