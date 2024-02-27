3762f40fa7320533fd7184f39566731ad506439e
IAM5K <sandeep2k2656@gmail.com>
2024-02-27 13:16:08 +0530

    Setup: Setup of Firebase function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,32 @@
-import * as functions from "firebase-functions";
-
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+import {onRequest} from "firebase-functions/v2/https";
+import * as cors from "cors";
+import * as logger from "firebase-functions/logger";
+
+// Configure CORS
+const corsHandler = cors({
+  origin: ["http://localhost:8100", "https://summarize-ng.web.app", "https://sumarize.web.app"],
+});
+
+export const helloWorld = onRequest((request, response) => {
+  // Allow CORS
+  corsHandler(request, response, () => {
+    const templateData = {
+      message: "Hello from Firebase!",
+      timestamp: new Date().toISOString(),
+    };
+
+    logger.info("Hello logs!", {structuredData: true});
+
+    response.status(200).json({data: templateData});
+  });
 });
