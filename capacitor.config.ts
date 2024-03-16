0a350a29aeb57937cac6953ab55c3cedd556e7bc
IAM5K <sandeep2k2656@gmail.com>
2024-03-16 19:37:31 +0530

    Android: Regenerate build gradle

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "app.web.summarize-ng",
+  appId: "app.web.summarize",
   appName: "summarize",
   webDir: "www",
   bundledWebRuntime: false,
