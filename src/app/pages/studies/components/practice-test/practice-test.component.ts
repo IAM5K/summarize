d22633e01eaf738f4182bb6104db1be55beefdb9
IAM5K <sandeep2k2656@gmail.com>
2024-04-03 08:10:02 +0530

    new: practice test

---
+++ src/app/pages/studies/components/practice-test/practice-test.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-practice-test',
+  templateUrl: './practice-test.component.html',
+  styleUrls: ['./practice-test.component.scss'],
+})
+export class PracticeTestComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
