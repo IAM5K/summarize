087cbe5f14980e17e6caeaf294b0a904a1c85500
IAM5K <sandeep2k2656@gmail.com>
2024-04-03 08:10:33 +0530

    new: analyze study component added

---
+++ src/app/pages/studies/components/analyze-study/analyze-study.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-analyze-study',
+  templateUrl: './analyze-study.component.html',
+  styleUrls: ['./analyze-study.component.scss'],
+})
+export class AnalyzeStudyComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
