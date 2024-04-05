603a0494e9c08ba898bd685be94af0a0d8233317
IAM5K <sandeep2k2656@gmail.com>
2024-04-05 15:20:31 +0530

    new: dynamic content in home page

--- src/app/shared/db/real-time-data-base.service.ts
+++ src/app/shared/db/real-time-data-base.service.ts
@@ -13,25 +13,28 @@ export class RealTimeDataBaseService {
     return this.db.list<any>(this.dashboardContent).valueChanges();
   }
 
-  addTemporaryData(): Promise<void> {
-    const paragraphs = [
-      "Summarize is an app that helps you to manage your Time, Money, and work which includes but is not limited to subject-wise studies, notes, to-do's, or office works.",
-      "Thinking when to use Summarize? To live better and managed, one should recall what they have done throughout the day daily before sleep . Also listing tasks for the next day and setting priorities increases the chances to get them done.",
-      "Currently, Summarize is under development, we will keep on adding features one by one. Till then, please feel free to manage Achievements of day, Expenses, Studies, and Time. It is a web-app that can be easily installed on all devices ( Mobile & PC ). Install the app on your devices  to keep in sync and to check how to install visit our Help page.",
-      "Note: Your data will not be used for any advertisement or offering any deal/scheme. Also our app donot need any permission to work on your device. Your privacy is important to us. For more queries, you can contact us through the support page. We are also open to feature requests and suggestions.",
-    ];
-    const paragraphsData = paragraphs.map((paragraph, index) => {
-      return {
-        content: paragraph,
-        orderId: index + 1,
-        type: "text", // or 'note' based on your condition
-      };
-    });
+  // addTemporaryData(): Promise<void> {
+  //   const paragraphs = [
+  //     "Summarize is an app that helps you to manage your Time, Money, and work which includes but is not limited to subject-wise studies, notes, to-do's, or office works.",
+  //     "Thinking when to use Summarize? To live better and managed, one should recall what they have done throughout the day daily before sleep . Also listing tasks for the next day and setting priorities increases the chances to get them done.",
+  //     "Currently, Summarize is under development, we will keep on adding features one by one. Till then, please feel free to manage Achievements of day, Expenses, Studies, and Time. It is a web-app that can be easily installed on all devices ( Mobile & PC ). Install the app on your devices  to keep in sync and to check how to install visit our Help page.",
+  //     "Note: Your data will not be used for any advertisement or offering any deal/scheme. Also our app donot need any permission to work on your device. Your privacy is important to us. For more queries, you can contact us through the support page. We are also open to feature requests and suggestions.",
+  //   ];
+  //   const paragraphsData = paragraphs.map((paragraph, index) => {
+  //     return {
+  //       content: paragraph,
+  //       orderId: index + 1,
+  //       type: "text",
+  //     };
+  //   });
 
-    return this.db.object("dynamicContent/home/paragraphs").set(paragraphsData);
+  //   return this.db.object("dynamicContent/dashboard/paragraphs").set(paragraphsData);
+  // }
+
+  getHomeData(path: string): Observable<any> {
+    return this.db.object(`dynamicContent/dashboard/${path}`).valueChanges();
   }
-
-  getHomeData(): Observable<any> {
-    return this.db.object("dynamicContent/home").valueChanges();
+  getTargetExam(): Observable<any> {
+    return this.db.object("targetExams").valueChanges();
   }
 }
