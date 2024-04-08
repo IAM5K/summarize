b316243ed4403ab995e7dc317eb34bcf84388444
IAM5K <sandeep2k2656@gmail.com>
2024-04-08 06:30:36 +0530

    format: format through linting

--- src/app/pages/studies/components/advanced-studies/advanced-studies.component.ts
+++ src/app/pages/studies/components/advanced-studies/advanced-studies.component.ts
@@ -6,5 +6,4 @@ import { Component } from "@angular/core";
   styleUrls: ["./advanced-studies.component.scss"],
 })
 export class AdvancedStudiesComponent {
-  constructor() {}
 }
--- src/app/pages/studies/components/edit-studies/edit-studies.component.ts
+++ src/app/pages/studies/components/edit-studies/edit-studies.component.ts
@@ -6,5 +6,4 @@ import { Component } from "@angular/core";
   styleUrls: ["./edit-studies.component.scss"],
 })
 export class EditStudiesComponent {
-  constructor() {}
 }
