8ebdeba33fb548be3ce4915214d95495715721e4
IAM5K <sandeep2k2656@gmail.com>
2024-04-08 06:30:36 +0530

    format: format through linting

--- src/app/pages/goal/components/goal-info/goal-info.component.ts
+++ src/app/pages/goal/components/goal-info/goal-info.component.ts
@@ -1,16 +1,14 @@
-import { Component, OnInit } from "@angular/core";
-import { GoalData } from "src/app/models/class/static/goal";
-import { DataDescription } from "src/app/models/interface/masterData.model";
+import { Component, OnInit } from '@angular/core';
+import { GoalData } from 'src/app/models/class/static/goal';
+import { DataDescription } from 'src/app/models/interface/masterData.model';
 
 @Component({
-  selector: "app-goal-info",
-  templateUrl: "./goal-info.component.html",
-  styleUrls: ["./goal-info.component.scss"],
+  selector: 'app-goal-info',
+  templateUrl: './goal-info.component.html',
+  styleUrls: ['./goal-info.component.scss'],
 })
 export class GoalInfoComponent {
-  title: string = "Goal Setting";
+  title: string = 'Goal Setting';
   goalSetting: DataDescription[] = GoalData.goalSetting;
   goalDetails: DataDescription[] = GoalData.goalDetails;
-
-  constructor() {}
 }
