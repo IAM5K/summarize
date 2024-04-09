5a794a3a9158e7fa03591af1698a60b038df8bf3
IAM5K <sandeep2k2656@gmail.com>
2024-04-09 11:36:20 +0530

    format: linting after setup

--- src/app/pages/studies/components/practice-test/practice-test.component.ts
+++ src/app/pages/studies/components/practice-test/practice-test.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-practice-test',
-  templateUrl: './practice-test.component.html',
-  styleUrls: ['./practice-test.component.scss'],
+  selector: "app-practice-test",
+  templateUrl: "./practice-test.component.html",
+  styleUrls: ["./practice-test.component.scss"],
 })
 export class PracticeTestComponent  implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    console.log("Practice component");
+
+  }
 
 }
