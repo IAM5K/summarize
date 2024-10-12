e0c941dba74eabe827045bedb5da274e853b0054
IAM5K <sandeep2k2656@gmail.com>
2024-10-12 19:22:29 +0530

    Add static data from home to static class

---
+++ src/app/models/data/home-page.data.ts
@@ -0,0 +1,14 @@
+export const homePageParagraphs = [
+  "Summarize is an app that helps you to manage your Time, Money, and work which includes but is not limited to subject-wise studies, notes, to-do's, or office works.",
+  "Thinking when to use Summarize? To live better and managed, one should recall what they have done throughout the day daily before sleep . Also listing tasks for the next day and setting priorities increases the chances to get them done.",
+  "Currently, Summarize is under development, we will keep on adding features one by one. Till then, please feel free to manage Achievements of day, Expenses, Studies, and Time. It is a web-app that can be easily installed on all devices ( Mobile & PC ). Install the app on your devices  to keep in sync and to check how to install visit our Help page.",
+  "Note: Your data will not be used for any advertisement or offering any deal/scheme. Also our app don't need any permission to work on your device. Your privacy is important to us. For more queries, you can contact us through the support page. We are also open to feature requests and suggestions.",
+];
+
+export const fabActionButtonsAtHome = [
+  { title: "Goal", color: "secondary", url: "/goal", icon: "bulb" },
+  { title: "Expenses", color: "success", url: "/expenses", icon: "cash" },
+  { title: "Studies", color: "primary", url: "/studies", icon: "book" },
+  { title: "Time", color: "danger", url: "/time", icon: "hourglass" },
+  { title: "Moments", color: "warning", url: "/moments", icon: "sparkles" },
+];
