9f60ca20ec2aec290b9ef942e1496a6e9d8aaf64
IAM5K <sandeep2k2656@gmail.com>
2025-03-29 21:10:07 +0530

    Add quick feature for whatsapp connect

---
+++ src/app/pages/home/components/quick-features/quick-features.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-quick-features",
+  templateUrl: "./quick-features.component.html",
+  styleUrls: ["./quick-features.component.scss"],
+})
+export class QuickFeaturesComponent implements OnInit {
+  phoneNumber: string = "";
+  countryCode: string = "+91";
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  openWhatsAppLink() {
+    if (this.phoneNumber) {
+      const url = `https://wa.me/${this.countryCode}${this.phoneNumber}`;
+      window.open(url, "_blank");
+    } else {
+      alert("Please enter a valid mobile number.");
+    }
+  }
+}
