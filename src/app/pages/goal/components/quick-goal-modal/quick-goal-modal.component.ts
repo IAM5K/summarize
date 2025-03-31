f07e8941d68fb1f79c55fa501695aa3d8450fb37
IAM5K <sandeep2k2656@gmail.com>
2025-03-31 11:00:43 +0530

    Create goal modal

---
+++ src/app/pages/goal/components/quick-goal-modal/quick-goal-modal.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quick-goal-modal',
+  templateUrl: './quick-goal-modal.component.html',
+  styleUrls: ['./quick-goal-modal.component.scss'],
+})
+export class QuickGoalModalComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
