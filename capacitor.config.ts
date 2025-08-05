8f9e2cb3824e3a7a9b4028320dd82d18e5e57905
IAM5K <sandeep2k2656@gmail.com>
2025-08-06 01:23:54 +0530

    chore: update android bundle name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
   appId: "app.web.summarize",
-  appName: "summarize",
+  appName: "Summarize",
   webDir: "www",
   bundledWebRuntime: false,
   plugins: {
