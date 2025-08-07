3d1fd9fef5e8c1f125af73d4b75874a9c8c47b16
IAM5K <sandeep2k2656@gmail.com>
2025-08-07 06:49:52 +0530

    feat: update quick goal module logic

--- src/app/pages/goal/components/quick-goal-modal/quick-goal-modal.component.ts
+++ src/app/pages/goal/components/quick-goal-modal/quick-goal-modal.component.ts
@@ -1,37 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { Component, OnInit } from "@angular/core";
+import { ModalController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-quick-goal-modal',
-  templateUrl: './quick-goal-modal.component.html',
-  styleUrls: ['./quick-goal-modal.component.scss'],
+  selector: "app-quick-goal-modal",
+  templateUrl: "./quick-goal-modal.component.html",
+  styleUrls: ["./quick-goal-modal.component.scss"],
 })
 export class QuickGoalModalComponent implements OnInit {
-  goalTitle: string = '';
-  goalDescription: string = '';
+  goalTitle: string = "";
+  goalDescription: string = "";
   actionSteps: string[] = [];
-  newActionStep: string = '';
+  newActionStep: string = "";
 
   constructor(private modalController: ModalController) {}
 
   ngOnInit() {}
 
   addActionStep() {
-    if (this.newActionStep.trim()) {
-      this.actionSteps.push(this.newActionStep.trim());
+    const step = this.newActionStep.trim();
+    if (step) {
+      this.actionSteps.push(step);
       this.newActionStep = '';
     }
   }
 
   removeActionStep(index: number) {
-    this.actionSteps.splice(index, 1);
+    if (index > -1 && index < this.actionSteps.length) {
+      this.actionSteps.splice(index, 1);
+    }
   }
 
   saveGoal() {
+    if (!this.goalTitle.trim()) return;
     const goalData = {
-      title: this.goalTitle,
-      description: this.goalDescription,
-      actionSteps: this.actionSteps,
+      title: this.goalTitle.trim(),
+      description: this.goalDescription.trim(),
+      actionSteps: [...this.actionSteps],
     };
     this.modalController.dismiss(goalData);
   }
--- src/app/pages/goal/goal.module.ts
+++ src/app/pages/goal/goal.module.ts
@@ -8,9 +8,10 @@ import { GoalPageRoutingModule } from "./goal-routing.module";
 
 import { GoalPage } from "./goal.page";
 import { GoalInfoComponent } from "./components/goal-info/goal-info.component";
+import { QuickGoalModalComponent } from "./components/quick-goal-modal/quick-goal-modal.component";
 
 @NgModule({
   imports: [CommonModule, FormsModule, IonicModule, GoalPageRoutingModule, ReactiveFormsModule],
-  declarations: [GoalPage, GoalInfoComponent],
+  declarations: [GoalPage, GoalInfoComponent, QuickGoalModalComponent],
 })
 export class GoalPageModule {}
